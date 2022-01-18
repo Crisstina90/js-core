becffade7e3e61f6323dc32931304563841d779a
crisstina90 <pavaloaiacristina90@gmail.com>
2022-01-18 20:52:56 +0200

    JS

---
+++ hello/hello.js
@@ -0,0 +1,16 @@
+
+// ruleaza tellMe dupa 3000 ms (3 secunde):
+setTimeout(tellMe, 3000)
+
+// creaza o noua instructiune (functie) numita tellMe
+function tellMe() {
+    alert("Hello world!")
+    alert("Welcome to JavaScript!")
+}
+
+/*
+comentariu p
+mai multe linii
+*/
+
+// comentariu pe o singura linie 
