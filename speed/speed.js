20a4ca074e1a461219e7b399113bfd6ba708dc84
crisstina90 <pavaloaiacristina90@gmail.com>
2022-02-17 20:20:22 +0200

    keypress speedtyping

--- speed/speed.js
+++ speed/speed.js
@@ -1,10 +1,13 @@
+
+const speed = 1000
+
 function randomNumer(max) {
     return Math.round(Math.random() * max)
 }
 
 
 function randomLetter() {
-    let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     let i = randomNumer(letters.length -1)
     return letters[i]
 }
@@ -13,7 +16,7 @@ function randomColor() {
     let red = randomNumer(255)
     let green = randomNumer(255)
     let blue = randomNumer(255)
-    return `rgb(${red}), rgb(${green}), rgb(${blue})` 
+    return `rgb(${red}, ${green}, ${blue})` 
 }
 
 function  randomLetterElement() {
@@ -22,15 +25,16 @@ function  randomLetterElement() {
     div.style.backgroundColor = randomColor()
     div.style.top = randomNumer(99) +  'vh'
     div.style.left = randomNumer(99) + 'vw'
-    div.classList.add('letter')
+    div.classList.add('letter', div.innerText)
     document.querySelector('body').append(div)
 }
 
 async function run() {
     var gameOver = false
+    hideStart()
     while(!gameOver) {
         randomLetterElement()
-        await new Promise(r => setTimeout(r, 2000))
+        await new Promise(r => setTimeout(r, speed))
     }
 }
 
@@ -38,5 +42,16 @@ function hideStart() {
     document.querySelector('input').style.display = 'none'
 }
 
-document.querySelector('input').addEventListener('click', run)
+function pressLetter(event) {
+    let code = event.keyCode
+    let letter = String.fromCharCode(code)
+    // gaseste primul element cu clasa literei apasate
+    let element = document.querySelector('.${letter}')
+    // daca elementul exista (nu este null) atunci il stergem din html
+    if (element) {
+        element.remove()
+    }
+}
 
+document.querySelector('input').addEventListener('click', run)
+document.addEventListener('keydown', pressLetter)
